e973cf04db5f86f280731ecce19bb3bb61941e71
MasterMaxLi <riwinkids@gmail.com>
2021-02-26 01:29:57 +0800

    changes

--- src/main/java/com/limingxuan/gitstudy/change/HelloJava.java
+++ src/main/java/com/limingxuan/gitstudy/change/HelloJava.java
@@ -1,10 +1,16 @@
 package com.limingxuan.gitstudy.change;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Creqated by Limingxuan on 2021/2/26
  */
+@RestController
 public class HelloJava {
-    public static void main(String[] args) {
-        System.out.println("Hello Java");
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello Java";
     }
 }
