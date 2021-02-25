508d22a6441aad36b4c3819e27d49fff21bc0383
MasterMaxLi <riwinkids@gmail.com>
2021-02-26 00:29:54 +0800

    修改程序

---
+++ src/main/java/com/limingxuan/gitstudy/change/HelloJava.java
@@ -0,0 +1,10 @@
+package com.limingxuan.gitstudy.change;
+
+/**
+ * Creqated by Limingxuan on 2021/2/26
+ */
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
